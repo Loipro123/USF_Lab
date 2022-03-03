5499a5417486bb7d66ac871fa35c56347150b67e
Loi Le <53159895+Loipro123@users.noreply.github.com>
2022-03-03 17:01:41 -0500

    update

--- javascript/login.js
+++ javascript/login.js
@@ -1,11 +1,10 @@
 
 const inputId = document.getElementById('user_id');
 const submitBtn = document.getElementById('submit_btn');
-let user_id = null;
 submitBtn.addEventListener('click', ()=> {
     const userId = inputId.value;
     if(userId.length > 0){
-      document.location.href  = `/page/home.html?userId=${user_id}`;
+      document.location.href  = `page/home.html?userId=${userId}`;
     }
     else{
         console.log('Input is empty!')
